53d5b9bfcfe13f409496a2157ccdd82a982f4cfe
Papika0 <papismedovi1@gmail.com>
2023-10-20 19:44:42 +0400

    refactor - format code

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,60 +1,66 @@
 <template>
-    <div class="flex flex-row justify-between">
-        <input class="hidden" type="checkbox" :id="todo.id" @change="toggleComplete">
-        <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900" :for="todo.id">
-            <span :class="{ 'bg-green-500 border-green-500': todo.completed }"
-                class="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
-                <svg class="w-4 h-4 fill-current" :class="{ 'text-white': todo.completed }"
-                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd" />
-                </svg>
-            </span>
-            <span class="ml-4 text-sm" @dblclick="editMode = true; $nextTick(() => $refs.editInput.focus())">
-                <input :class="{ 'line-through': todo.completed }" class="bg-transparent outline-none" ref="editInput"
-                    v-model="editedTitle" @keydown.enter="saveEdit" @keydown.esc="cancelEdit" :disabled="!editMode"
-                    :style="{ width: `${editedTitle.length}ch` }" />
-            </span>
-        </label>
+  <div class="flex flex-row justify-between">
+    <input class="hidden" type="checkbox" :id="todo.id" @change="toggleComplete" />
+    <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900" :for="todo.id">
+      <span :class="{ 'bg-green-500 border-green-500': todo.completed }"
+        class="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
+        <svg class="w-4 h-4 fill-current" :class="{ 'text-white': todo.completed }" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd"
+            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+            clip-rule="evenodd" />
+        </svg>
+      </span>
+      <span class="ml-4 text-sm" @dblclick="
+        editMode = true;
+      $nextTick(() => $refs.editInput.focus())
+        ">
+        <input :class="{ 'line-through': todo.completed }" class="bg-transparent outline-none" ref="editInput"
+          :id="todo.id + 'id'" v-model="editedTitle" @keydown.enter="saveEdit" @keydown.esc="cancelEdit"
+          :disabled="!editMode" :style="{ width: `${editedTitle.length}ch` }" />
+      </span>
+    </label>
 
-        <div class="inline-flex gap-5">
-            <button @click="editMode = true; $nextTick(() => $refs.editInput.focus())" v-if="!editMode">Edit</button>
-            <IconDeleteX class="w-4 h-4 my-auto cursor-pointer" @click="deleteItem">Delete</IconDeleteX>
-        </div>
+    <div class="inline-flex gap-5">
+      <button @click="
+        editMode = true;
+      $nextTick(() => $refs.editInput.focus())
+        " v-if="!editMode">
+        Edit
+      </button>
+      <IconDeleteX class="w-4 h-4 my-auto cursor-pointer" @click="deleteItem">Delete</IconDeleteX>
     </div>
+  </div>
 </template>
-  
-<script setup>
-import { ref, defineEmits } from 'vue';
-import IconDeleteX from '@/components/icons/IconDeleteX.vue';
 
-const emit = defineEmits();
+<script setup>
+import { ref, defineEmits } from 'vue'
+import IconDeleteX from '@/components/icons/IconDeleteX.vue'
+
+const emit = defineEmits()
 
 const props = defineProps({
-    todo: Object,
-});
+  todo: Object
+})
 
-const editMode = ref(false);
-const editedTitle = ref(props.todo.title);
+const editMode = ref(false)
+const editedTitle = ref(props.todo.title)
 
 const saveEdit = () => {
-    editMode.value = false;
-    emit('editTodo', { todo: props.todo, newTitle: editedTitle.value });
-};
+  editMode.value = false
+  emit('editTodo', { todo: props.todo, newTitle: editedTitle.value })
+}
 
 const cancelEdit = () => {
-    editMode.value = false;
-    editedTitle.value = props.todo.title;
-};
+  editMode.value = false
+  editedTitle.value = props.todo.title
+}
 
 const deleteItem = () => {
-    emit('deleteTodo', props.todo);
-};
+  emit('deleteTodo', props.todo)
+}
 
 const toggleComplete = () => {
-    emit('toggleComplete', props.todo);
-};
-
+  emit('toggleComplete', props.todo)
+}
 </script>
-  
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,25 +1,37 @@
 <template>
-    <div class="flex items-center justify-center w-screen h-screen font-medium">
-        <div class="flex flex-grow items-center justify-center h-full text-gray-600 bg-gray-100">
-            <div class="flex flex-grow items-center justify-center bg-gray-900 h-full">
-                <div class="max-w-full p-8 bg-gray-800 rounded-lg shadow-lg w-fit h-3/4 overflow-y-auto text-gray-200">
-                    <div class="flex items-center mb-3">
-                        <IconCard class="w-8 h-8" />
-                        <h4 class="font-semibold ml-3 text-lg">Todo List</h4>
-                    </div>
-                    <div class="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded mb-2">
-                        <IconPlus class="w-6 h-6 cursor-pointer" @click="addTodo" />
-                        <input class="flex-grow h-8 ml-4 bg-transparent focus:outline-none font-medium" type="text"
-                            id="newTodo" v-model="newTodo" @keydown.enter="addTodo" placeholder="add a new task" />
-                    </div>
-                    <div v-for="todo in todos" :key="todo.id">
-                        <TodoItem :todo="todo" @editTodo="editTodo" @deleteTodo="deleteTodo"
-                            @toggleComplete="toggleComplete" />
-                    </div>
-                </div>
-            </div>
+  <div class="flex items-center justify-center w-screen h-screen font-medium">
+    <div class="flex flex-grow items-center justify-center h-full text-gray-600 bg-gray-100">
+      <div class="flex flex-grow items-center justify-center bg-gray-900 h-full">
+        <div
+          class="max-w-full p-8 bg-gray-800 rounded-lg shadow-lg w-fit h-3/4 overflow-y-auto text-gray-200"
+        >
+          <div class="flex items-center mb-3">
+            <IconCard class="w-8 h-8" />
+            <h4 class="font-semibold ml-3 text-lg">Todo List</h4>
+          </div>
+          <div class="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded mb-2">
+            <IconPlus class="w-6 h-6 cursor-pointer" @click="addTodo" />
+            <input
+              class="flex-grow h-8 ml-4 bg-transparent focus:outline-none font-medium"
+              type="text"
+              id="newTodo"
+              v-model="newTodo"
+              @keydown.enter="addTodo"
+              placeholder="add a new task"
+            />
+          </div>
+          <div v-for="todo in todos" :key="todo.id">
+            <TodoItem
+              :todo="todo"
+              @editTodo="editTodo"
+              @deleteTodo="deleteTodo"
+              @toggleComplete="toggleComplete"
+            />
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -29,45 +41,45 @@ import IconCard from '@/components/icons/IconCard.vue'
 import IconPlus from '@/components/icons/IconPlus.vue'
 import axios from 'axios'
 
-const newTodo = ref('');
-const todos = ref([]);
+const newTodo = ref('')
+const todos = ref([])
 
 const addTodo = () => {
-    if (newTodo.value.trim() === '') return;
-    const newTodoItem = {
-        id: todos.value.length + 1,
-        title: newTodo.value,
-        completed: false,
-    };
-    todos.value.unshift(newTodoItem);
-    newTodo.value = '';
-};
+  if (newTodo.value.trim() === '') return
+  const newTodoItem = {
+    id: todos.value.length + 1,
+    title: newTodo.value,
+    completed: false
+  }
+  todos.value.unshift(newTodoItem)
+  newTodo.value = ''
+}
 
 const editTodo = (obj) => {
-    todos.value = todos.value.map((item) => {
-        if (item.id === obj.todo.id) {
-            item.title = obj.newTitle;
-        }
-        return item;
-    });
-};
+  todos.value = todos.value.map((item) => {
+    if (item.id === obj.todo.id) {
+      item.title = obj.newTitle
+    }
+    return item
+  })
+}
 
 const deleteTodo = (obj) => {
-    todos.value = todos.value.filter((item) => item.id !== obj.id);
-};
+  todos.value = todos.value.filter((item) => item.id !== obj.id)
+}
 
 const toggleComplete = (obj) => {
-    todos.value = todos.value.map((item) => {
-        if (item.id === obj.id) {
-            item.completed = !item.completed;
-        }
-        return item;
-    });
-};
+  todos.value = todos.value.map((item) => {
+    if (item.id === obj.id) {
+      item.completed = !item.completed
+    }
+    return item
+  })
+}
 
 onBeforeMount(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos').then((response) => {
-        todos.value = response.data;
-    });
-});
+  axios.get('https://jsonplaceholder.typicode.com/todos').then((response) => {
+    todos.value = response.data
+  })
+})
 </script>
