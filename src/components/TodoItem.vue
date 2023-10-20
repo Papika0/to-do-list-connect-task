8ec63ecfdb4963fdb4d96abe07af3c6995d9bb7e
Papika0 <papismedovi1@gmail.com>
2023-10-20 17:16:25 +0400

    feat - to do item component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="flex flex-row justify-between">
+        <input class="hidden" type="checkbox" :id="todo.id" @change="toggleComplete">
+        <label class="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900 w-fit" :for="todo.id">
+            <span :class="{ 'bg-green-500 border-green-500': todo.completed }"
+                class="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
+                <svg class="w-4 h-4 fill-current" :class="{ 'text-white': todo.completed }"
+                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd" />
+                </svg>
+            </span>
+            <span class="ml-4 text-sm" :class="{ 'line-through': todo.completed }"
+                @dblclick="editMode = true; $nextTick(() => $refs.editInput.focus())">
+                <span v-if="!editMode">{{ todo.title }}</span>
+                <input class=" bg-transparent outline-none" ref="editInput" v-if="editMode" v-model="editedTitle"
+                    @keydown.enter="saveEdit" @keydown.esc="cancelEdit" />
+            </span>
+        </label>
+        <div class="inline-flex gap-5">
+            <button @click="editMode = true" v-if="!editMode">Edit</button>
+            <button @click="deleteItem">Delete</button>
+        </div>
+    </div>
+</template>
+  
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits();
+
+const props = defineProps({
+    todo: Object,
+});
+
+const editMode = ref(false);
+const editedTitle = ref(props.todo.title);
+
+const saveEdit = () => {
+    editMode.value = false;
+    emit('editTodo', { todo: props.todo, newTitle: editedTitle.value });
+};
+
+const cancelEdit = () => {
+    editMode.value = false;
+    editedTitle.value = props.todo.title;
+};
+
+const deleteItem = () => {
+    emit('deleteTodo', props.todo);
+};
+
+const toggleComplete = () => {
+    emit('toggleComplete', props.todo);
+};
+
+</script>
+  
