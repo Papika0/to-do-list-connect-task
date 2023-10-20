0efc95472210af48cdb69a131544c6cef711dbd8
Papika0 <papismedovi1@gmail.com>
2023-10-20 17:15:16 +0400

    feat - make router / tailwind config

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import TodoList from '@/views/TodoList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: TodoList
     },
   ]
 })
